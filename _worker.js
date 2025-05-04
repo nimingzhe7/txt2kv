e983130aed77d0a58058e3890d5fbfcc16824ba7
nimingzhe7 <38258088+nimingzhe7@users.noreply.github.com>
2025-05-04 09:07:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,4 @@
 /**
- * YouTube Channel: https://youtube.com/@am_clubs
- * Telegram Group: https://t.me/am_clubs
- * GitHub Repository: https://github.com/amclubs
- * Personal Blog: https://amclubs.blogspot.com
- * Personal Blog: https://amclubss.com
  */
 
 let myToken = 'pwd';
